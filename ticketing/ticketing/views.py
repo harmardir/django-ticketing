33a67cf5ed644480826751ebc6dc74ab2b3975eb
Harout <harmardir@gmail.com>
2022-09-20 23:58:29 +0300

    creating model , adding data and view dynamic data

--- ticketing/ticketing/views.py
+++ ticketing/ticketing/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from ticketing.models import Ticket
 
 def index(request):
     return render(request,'index.html')
 
 def submit(request):
+    new_ticket = Ticket(submitter='Test User', body='Help ! , I need help with a bug')
+    new_ticket.save()
     return render(request,'submit.html')
 
 def tickets(request):
-    return render(request,'tickets.html')
+    all_tickets = Ticket.objects.all()
+    return render(request,'tickets.html',{'tickets':all_tickets})
